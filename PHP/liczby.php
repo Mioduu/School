eead8f48a5daa5a716bd0ea753b33fa832e6a503
Mioduu <miodubusiness@gmail.com>
2026-09-09 09:47:59 +0200

    Dodany PHP starcik

---
+++ PHP/liczby.php
@@ -0,0 +1,13 @@
+<?php
+    $liczba1 = 42;
+    print("Liczba to: " . $liczba1);
+
+    $liczbaOctalna = 042;
+    print("<br>Liczba osemkowa to: " . $liczbaOctalna);
+    $liczbaHex = 0x2A;
+    print("<br>Liczba szesnastkowa to: " . $liczbaHex);
+    $liczbaBin = 0b101010;
+    print("<br>Liczba binarna to: " . $liczbaBin);
+    $liczbaDuza = 1_234_567;
+    print("<br>Liczba duza to: " . $liczbaDuza);
+?>
